6e55d054e0c61f0fed7d625fc741a22ee543de8d
Rabiya Aqeel <rabiyaghouri@ymail.com>
2023-08-15 23:56:25 +0400

    Initial commit

---
+++ src/main/java/com/rabiya/freemarker/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.rabiya.freemarker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rabiya.freemarker.model.Employee;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
